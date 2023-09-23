aacadba509e9f25931e2fee4c3e145dd397548e8
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-23 10:00:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
+const texts = document.querySelectorAll('strong')
 function highlight() {
-    //Write your code here
-
-
+  //Write your code here
+  texts.forEach(el=>el.style.color="green")
 }
 
 
 function return_normal() {
-    //Write your code here
+  //Write your code here
 
-    
+  texts.forEach(el=>el.style.color="black")
+  
 }
